d9a04a23fc5d0deac1201ef219caf8a21f0442df
ydemetriades <yiannis.demetriades@gmail.com>
2021-10-19 15:24:05 +0300

    fix xcode13 kingfisher support version 6
    see: onevcat/kingfisher branch version6-xcode13

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "KingfisherWebP", targets: ["KingfisherWebP"])
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.3.1"),
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode", from: "1.1.0")
     ],
     targets: [
